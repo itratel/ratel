80156eac9e22e91c9f1f6caf80aeb95b69915ad8
wuhaodong <wuhaodong@hikvision.com>
2019-10-22 16:26:28 +0800

    initial commit

---
+++ ratel-registry/src/main/java/com/itratel/ratel/registry/RatelRegistryApplication.java
@@ -0,0 +1,26 @@
+package com.itratel.ratel.registry;
+
+import com.itratel.ratel.registry.annotation.EnableRegistryServer;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author whd.java@gmail.com
+ * @version 0.0.1
+ * @date 2019/10/22 14:37
+ * @apiNote Describe the function of this class in one sentence
+ * <p>
+ * {@link @SpringCloudApplication} is composition annotation that function be equivalent to three annotation as below
+ * @see @SpringBootApplication
+ * @see @EnableDiscoveryClient
+ * @see @EnableCircuitBreaker
+ * </p>
+ * @since 0.0.1
+ */
+@EnableRegistryServer
+public class RatelRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatelRegistryApplication.class, args);
+    }
+
+}
---
+++ ratel-registry/src/main/java/com/itratel/ratel/registry/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.itratel.ratel.registry.security;
+
+import lombok.SneakyThrows;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author whd.java@gmail.com
+ * @version 0.0.1
+ * @date 2019/10/22 15:00
+ * @apiNote Describe the function of this class in one sentence
+ * @since 0.0.1
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @SneakyThrows
+    protected void configure(HttpSecurity http) {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
