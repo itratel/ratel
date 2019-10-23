37dccc9b4b48d2ace3e856650e8ea9b2a62dbc6f
wuhaodong <wuhaodong@hikvision.com>
2019-10-23 18:19:31 +0800

    add datasource init and oauth2 init

---
+++ ratel-authorization/ratel-authorization-oauth2/ratel-authorization-oauth2-authserver/src/main/java/org/ratelframework/ratel/authorization/oauth2/server/config/AuthorizationServerConfig.java
@@ -0,0 +1,74 @@
+package org.ratelframework.ratel.authorization.oauth2.server.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @author whd.java@gmail.com
+ * @version 0.0.1
+ * @date 2019/10/23 18:06
+ * @apiNote Describe the function of this class in one sentence
+ * @since 0.0.1
+ */
+@Configuration
+@EnableAuthorizationServer
+@RequiredArgsConstructor(onConstructor__={@Autowired})
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final String DEMO_RESOURCE_ID = "order";
+
+    private final AuthenticationManager authenticationManager;
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //配置两个客户端,一个用于password认证一个用于client认证
+        clients.inMemory().withClient("client_1")
+                .resourceIds(DEMO_RESOURCE_ID)
+                .authorizedGrantTypes("client_credentials")
+                .scopes("select")
+                .authorities("oauth2")
+                .secret("123456")
+                .and().withClient("client_2")
+                .resourceIds(DEMO_RESOURCE_ID)
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("select")
+                .authorities("oauth2")
+                .secret("123456");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(new RedisTokenStore(redisConnectionFactory))
+                .tokenStore(new InMemoryTokenStore())
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                // 2018-4-3 增加配置，允许 GET、POST 请求获取 token，即访问端点：oauth/token
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+
+        endpoints.reuseRefreshTokens(true);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        //允许表单认证
+        oauthServer.allowFormAuthenticationForClients();
+    }
+
+}
---
+++ ratel-common/ratel-common-datasource/src/main/java/org/ratelframework/ratel/common/annotation/CandidateDataSource.java
@@ -0,0 +1,24 @@
+package org.ratelframework.ratel.common.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+import static org.ratelframework.ratel.common.annotation.CandidateDataSource.NAME;
+
+/**
+ * @author whd.java@gmail.com
+ * @date 2019/10/22 15:03
+ * @apiNote Annotation to mark a DataSource bean to be configured to use a first choose.
+ * @since 0.0.1
+ * @version 0.0.1
+ */
+@Target({ ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Qualifier(value = NAME)
+public @interface CandidateDataSource {
+
+    String NAME = "candidateDataSource";
+}
