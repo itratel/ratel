70c8fbf1a9e0106a61db447037936d454cdb35f7
whd <whd.java@gmail.com>
2019-11-05 22:01:42 +0800

    add user controller

--- ratel-upms/ratel-upms-core/src/main/java/org/ratelframework/ratel/upms/controller/SysUserController.java
+++ ratel-upms/ratel-upms-core/src/main/java/org/ratelframework/ratel/upms/controller/SysUserController.java
@@ -1,11 +1,14 @@
 package org.ratelframework.ratel.upms.controller;
 
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.RequiredArgsConstructor;
+import org.ratelframework.ratel.common.core.utils.ResponseResult;
+import org.ratelframework.ratel.security.utils.SecurityUtils;
+import org.ratelframework.ratel.upms.api.entity.SysUser;
+import org.ratelframework.ratel.upms.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -20,4 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/upms/user")
 public class SysUserController {
 
+
+    private final ISysUserService userService;
+
+    /**
+     * 获取当前用户全部信息
+     *
+     * @return 用户信息
+     */
+    @GetMapping(value = {"/info"})
+    public ResponseResult info() {
+        String username = SecurityUtils.getUser().getUsername();
+        SysUser user = userService.getOne(Wrappers.<SysUser>query()
+                .lambda().eq(SysUser::getUsername, username));
+        if (user == null) {
+            return ResponseResult.error("获取当前用户信息失败");
+        }
+        return null;
+//        return ResponseResult.ok(userService.getUserInfo(user));
+    }
 }
