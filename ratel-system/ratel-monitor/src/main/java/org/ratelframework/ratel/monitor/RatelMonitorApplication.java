6bc6be6e2b2bed335b498b0c46895f8f48b3fb3f
whd <whd.java@gmail.com>
2019-11-16 01:03:35 +0800

    add admin server

--- ratel-system/ratel-monitor/src/main/java/org/ratelframework/ratel/monitor/RatelMonitorApplication.java
+++ ratel-system/ratel-monitor/src/main/java/org/ratelframework/ratel/monitor/RatelMonitorApplication.java
@@ -4,6 +4,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author whd.java@gmail.com
@@ -19,5 +22,15 @@ public class RatelMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatelMonitorApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
     }
 }
