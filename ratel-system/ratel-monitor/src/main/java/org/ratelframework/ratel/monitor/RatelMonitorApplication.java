afcc031cf18f268ce2173a639bdca252bb1d7cd2
whd <whd.java@gmail.com>
2019-11-19 23:56:34 +0800

    add monitor httpBasic login

--- ratel-system/ratel-monitor/src/main/java/org/ratelframework/ratel/monitor/RatelMonitorApplication.java
+++ ratel-system/ratel-monitor/src/main/java/org/ratelframework/ratel/monitor/RatelMonitorApplication.java
@@ -1,5 +1,6 @@
 package org.ratelframework.ratel.monitor;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  * @author whd.java@gmail.com
@@ -22,15 +24,5 @@ public class RatelMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatelMonitorApplication.class, args);
-    }
-
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
-        }
     }
 }
