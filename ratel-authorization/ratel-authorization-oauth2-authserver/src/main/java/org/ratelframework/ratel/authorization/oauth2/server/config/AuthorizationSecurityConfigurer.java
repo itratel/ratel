efc6eed3c9419326d7a49a3e3c065a5d580746ee
itratel <whd.java@gmail.com>
2020-11-27 01:15:18 +0800

    Modify config

--- ratel-authorization/ratel-authorization-oauth2-authserver/src/main/java/org/ratelframework/ratel/authorization/oauth2/server/config/AuthorizationSecurityConfigurer.java
+++ ratel-authorization/ratel-authorization-oauth2-authserver/src/main/java/org/ratelframework/ratel/authorization/oauth2/server/config/AuthorizationSecurityConfigurer.java
@@ -3,9 +3,7 @@ package org.ratelframework.ratel.authorization.oauth2.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,14 +13,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @apiNote Describe the function of this class in one sentence
  */
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class AuthorizationSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     /***
      * 获取授权码的接口方法
      * GET  http://localhost:8082/authserver/oauth/authorize?client_id=client1&response_type=code
-     * &scope-all&redirect_uri=http://www.baidu.com
+     * &scope=all&redirect_uri=http://www.baidu.com
      */
 
     /***
@@ -71,9 +67,8 @@ public class AuthorizationSecurityConfigurer extends WebSecurityConfigurerAdapte
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/r/**").hasAnyAuthority("p1")
-                .antMatchers("/login*").authenticated()
-                .anyRequest().permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin();
     }
