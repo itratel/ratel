67ee3ea96da183224d306addccb757ddb1ab0851
itratel <whd.java@gmail.com>
2020-11-30 12:29:14 +0800

    Change connection poll from druid to hikari

--- ratel-authorization/ratel-authorization-oauth2-client/src/main/java/org/ratelframework/ratel/authorization/oauth2/client/config/ClientWebSecurityConfigurer.java
+++ ratel-authorization/ratel-authorization-oauth2-client/src/main/java/org/ratelframework/ratel/authorization/oauth2/client/config/ClientWebSecurityConfigurer.java
@@ -34,7 +34,6 @@ public class ClientWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
                 .anyRequest().authenticated();
     }
 }
